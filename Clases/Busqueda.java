774fab9dd4a6a6244f2f97f9e207d0119c89abcf
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2023-11-26 14:30:39 -0600

    Update and rename Historial.java to Busqueda.java

---
+++ Clases/Busqueda.java
@@ -0,0 +1,12 @@
+public class Busqueda{
+    private Arbol historial;
+    
+    public Busqueda(String nombre, String provincia, String canton, int edad, String genero, String estadoCivil, String pelo, int altura, Lista sospechosos){
+        while (sospechosos.getCabeza()!=null){
+            //recorrer toda la lista, mientras recorre la lista ir calculando los puntos e ir armando el arbol binario
+        }
+        
+        //calcular puntos
+        //hacer arbol binario
+    }
+}
