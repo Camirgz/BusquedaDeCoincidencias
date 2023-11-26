a3e65e8043caa77697732fe56fab84a53469db14
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2023-11-26 11:22:49 -0600

    Update Interaccion.java

--- Clases/Interaccion.java
+++ Clases/Interaccion.java
@@ -71,7 +71,7 @@ public class Interaccion{
         System.out.println("Ingrese altura por buscar: ");
         String altura = scanner.nextLine();
         
-        
+        //metodo para hacer el arbol binario a partir del sistema de puntos.
         
         while(true){
             System.out.println("Ingrese [S] para salir, [B] para buscar o [H] para ver el historial");
