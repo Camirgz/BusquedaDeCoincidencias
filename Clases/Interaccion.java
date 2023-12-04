19ab9d02f88f037da60505d089ce6fd8a68e0ab4
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2023-12-04 12:54:16 -0600

    Update Interaccion.java

--- Clases/Interaccion.java
+++ Clases/Interaccion.java
@@ -45,13 +45,13 @@ public class Interaccion{
             }
         }
         
-        System.out.println("El archivo fue validado\n");
+        System.out.println("El archivo fue validado");
     }
     
     public String[] PedirEntradas(){
         Scanner scanner = new Scanner (System.in);
         
-        System.out.println("Ingrese los datos del sospechoso/a por buscar");
+        System.out.println("\nIngrese los datos del sospechoso/a por buscar");
         
         String nombre;
         String provincia;
@@ -132,23 +132,49 @@ public class Interaccion{
     
     public void MenuPrincipal (){
         Scanner scanner = new Scanner (System.in);
+        int cantidad_busquedas = 0;
         
         while(true){
+            System.out.println("\n== MENU PRINCIPAL ==");
             System.out.println("Ingrese [S] para salir, [B] para buscar o [H] para ver el historial");
             String entrada = scanner.nextLine();
             if(input(entrada)){
                 if(entrada.equalsIgnoreCase("B")){
                     String [] entradas = PedirEntradas();
-                    historial.agregarHistorial(sospechosos.Busqueda(entradas, sospechosos));
+                    cantidad_busquedas++;
+                    String print = "" + entradas[0] + ", " + entradas[1] + ", " + entradas[2] + ", " + entradas[3] + ", " + entradas[4] + ", " + entradas[5] + ", " + entradas[6] + ", " + entradas[7];
+                    print = print.toUpperCase();
+                    System.out.println("\n== RESULTADOS DE MEJOR A PEOR COINCIDENCIA ==");
+                    historial.agregarHistorial(sospechosos.Busqueda(entradas, sospechosos), print, cantidad_busquedas);
                     //metodo para hacer el arbol binario a partir del sistema de puntos.
+                    System.out.println("-Fin------------------------------------------");
                     //agregarMenuSecundario
                 }
                 if (entrada.equalsIgnoreCase("H")){
-                    historial.imprimirHistorial();
+                    if (cantidad_busquedas==1){
+                        System.out.println("\nSe ha realizado 1 búsqueda");
+                    } else{
+                        System.out.println("\nSe han realizado " + cantidad_busquedas + " búsquedas");
+                    }
+                    historial.imprimirBusqueda();
                     //metodo para mostrar la lista de historial.
+                    
+                    if (cantidad_busquedas>0){
+                        String numero_busqueda;
+                        while (true){
+                            System.out.println("\nIngrese el numero de búsqueda para ver los resultados arrojados: ");
+                            numero_busqueda = scanner.nextLine();
+                            if (numero_busqueda(numero_busqueda, cantidad_busquedas)){
+                                break;
+                            }
+                        }
+                        System.out.println("\n== RESULTADOS DE MEJOR A PEOR COINCIDENCIA ==");
+                        historial.imprimirHistorial(Integer.parseInt(numero_busqueda));
+                        System.out.println("-Fin------------------------------------------");
+                    }
                 }
                 if (entrada.equalsIgnoreCase("S")){
-                    System.out.println("Gracias!");
+                    System.out.println("\n== FIN DEL PROGRAMA ==");
                     break;
                 }
             }
@@ -162,6 +188,18 @@ public class Interaccion{
             }
         }
         return true;
+    }
+    
+    public boolean numero_busqueda(String numero, int cantidad_busquedas){
+        try{
+            int numero_busqueda = Integer.parseInt(numero);
+            if (numero_busqueda>0 && numero_busqueda<=cantidad_busquedas){
+                return true;
+            }
+            return false;
+        } catch (NumberFormatException e){
+            return false;
+        }
     }
 
     public boolean edad(String edadStr) {
@@ -274,9 +312,5 @@ public class Interaccion{
         } catch (Exception e) {
             return false;
         }
-    }
-    
-        public void mostrarHistorial(){
-        System.out.println("Historial");
     }
 }
